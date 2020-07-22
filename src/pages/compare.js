65d6a0b43edd27a70d9db0e4a9b7b110171bd65e
Hunter Evanoff <hunter547@gmail.com>
2020-07-21 22:23:59 -0400

    Working on the logic for the compare page to dynamically import the correct keyboard jsx component based on the keyboards passed to the page.

--- src/pages/compare.js
+++ src/pages/compare.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Wrapper from '../components/wrapper';
  
+
+function loadComponent(name) {
+  const Component = React.lazy(() =>
+    import(`../components/${name}.jsx`)
+  );
+  return Component;
+}
+
 const Compare = ({ location }) => {
 
-return (
-  <h1>Comparing {location.state.size} to a {location.state.keyboard}</h1>
-)
+  const componentFinder = [{size:'60%', component:'Sixty'},{size: '65%', component:'SixtyFive'},{size: '75%', component: 'Seventy'},{size: '80%', component: 'Eighty'},{size: '100%', component: 'Hundred'}];
+
+  const keyboard1 = componentFinder.forEach( component => {
+    if (component.size === location.state.size) {
+      return component.component;
+    }
+  });
+
+  console.log(keyboard1);
+
+  return (
+    <Wrapper>
+      <h1>Comparing {location.state.size ? location.state.size : null} to a {location.state.keyboard}</h1>
+    </Wrapper>
+  )
   
 }
  
